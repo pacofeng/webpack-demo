e5b75a18e2596b886e07735269d3bf89bc4d62e6
pacofeng <pacofeng86@gmail.com>
2022-10-23 21:42:04 -0700

    13 - Code Splitting

--- src/component.js
+++ src/component.js
@@ -2,5 +2,12 @@ export default (text = 'hello world') => {
   const element = document.createElement('div');
   element.innerHTML = text;
   element.className = 'rounded bg-red-100 border max-w-md m-4 p-4';
+  element.onclick = () => {
+    import('./lazy')
+      .then((lazy) => {
+        element.textContent = lazy.default;
+      })
+      .catch((err) => console.log(err));
+  };
   return element;
 };
