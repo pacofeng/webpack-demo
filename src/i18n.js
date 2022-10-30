e194757d4cfce1b5bfdcd0aabdbdbfeb76f986b1
pacofeng <pacofeng86@gmail.com>
2022-10-30 11:16:33 -0700

    29 - Internationalization

---
+++ src/i18n.js
@@ -0,0 +1,36 @@
+import 'regenerator-runtime/runtime';
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const [language, setLanguage] = useState('en');
+  const [hello, setHello] = useState('');
+
+  const changeLanguage = () => setLanguage(language === 'en' ? 'cn' : 'en');
+
+  useEffect(() => {
+    translate(language, 'hello').then(setHello).catch(console.error);
+  }, [language]);
+
+  return (
+    <div>
+      <button onClick={changeLanguage}>Change language</button>
+      <div>{hello}</div>
+    </div>
+  );
+};
+
+function translate(locale, text) {
+  return getLocaleData(locale).then((messages) => messages[text]);
+}
+
+async function getLocaleData(locale) {
+  return import(`../translations/${locale}.json`);
+}
+
+const root = document.createElement('div');
+
+root.setAttribute('id', 'app');
+document.body.appendChild(root);
+
+ReactDOM.render(<App />, root);
