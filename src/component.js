373c04a112e916550bdcf13d62745342d1599f5d
pacofeng <pacofeng86@gmail.com>
2022-10-30 10:59:47 -0700

    28 - Web Workers

--- src/component.js
+++ src/component.js
@@ -1,6 +1,13 @@
 export default (text = HELLO) => {
   const element = document.createElement('div');
-  element.innerHTML = text;
+  const worker = new Worker(new URL('./worker.js', import.meta.url));
+  const state = { text };
+
+  worker.addEventListener('message', ({ data: { text } }) => {
+    state.text = text;
+    element.innerHTML = text;
+  });
+  element.innerHTML = state.text;
   element.className = 'rounded bg-red-100 border max-w-md m-4 p-4';
   element.onclick = () => {
     import('./lazy')
@@ -9,5 +16,6 @@ export default (text = HELLO) => {
       })
       .catch((err) => console.log(err));
   };
+  element.onmouseenter = () => worker.postMessage({ text: state.text });
   return element;
 };
